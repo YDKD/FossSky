6d3f4304e0f1725d7f4e4564a5eb43a1ad611348
YDKD <1606354057@qq.com>
2022-01-24 16:45:41 +0800

    build(pinia): add pinia

---
+++ src/store/modules/user.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+interface UserInfo {
+  name: string
+}
+
+export const useUserStore = defineStore('user', {
+  state: (): UserInfo => ({
+    name: 'YDKD'
+  })
+})
+
+export function setupUser() {
+  useUserStore()
+}
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>Main</div>
+  <h1>{{ name }}</h1>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { useUserStore } from 'store/modules/user'
+const user = useUserStore()
+const { name } = storeToRefs(user)
+</script>
 
 <style scoped></style>
