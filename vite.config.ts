f2f957200715075d1e1b0a1e322092d7a784df6e
YDKD <1606354057@qq.com>
2022-01-24 12:01:25 +0800

    build(vite): update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
-
-const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      'components': '@/components'
-    }
-  }
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      },
+      {
+        find: 'components',
+        replacement: path.resolve(__dirname, 'src/components')
+      }
+    ]
+  },
+  plugins: [vue()]
 })
