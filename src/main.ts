160f8e7645c408b000cb0074f4074c938e1ae078
YDKD <1606354057@qq.com>
2022-01-26 10:11:10 +0800

    fix(element-plus): style import

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'element-plus/dist/index.css'
+
 import router from './router'
 import { store, setupStore } from './store'
 
 import globalRegister from './global'
-
 const app = createApp(App)
 
 app.use(globalRegister)
