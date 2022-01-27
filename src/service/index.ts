43e790a9dd0d9b72ffe2913376e166617cf1f099
YDKD <1606354057@qq.com>
2022-01-27 14:47:03 +0800

    feat(request): add request interceptors

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,7 +4,25 @@ import { BASE_URL, TIMEOUT } from './request/config'
 // 可能这里会有不同的选项配置，会导出不同的请求实例
 const defaultRequest = new FossRequest({
   baseURL: BASE_URL,
-  timeout: TIMEOUT
+  timeout: TIMEOUT,
+  interceptors: {
+    requestInterceptor: (config) => {
+      console.log('请求拦截')
+      return config
+    },
+    requestInterceptorCatch: (config) => {
+      console.log('请求错误')
+      return config
+    },
+    reponseInterceptor: (config) => {
+      console.log('响应拦截')
+      return config
+    },
+    reponseInterceptorCatch: (config) => {
+      console.log('响应错误')
+      return config
+    }
+  }
 })
 
 export { defaultRequest }
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,10 +1,23 @@
 import axios from 'axios'
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { FossRequestConfig, FossRequestInterceptor } from './type'
 class FossRequest {
   requestInstance: AxiosInstance
+  interceptor?: FossRequestInterceptor
 
-  constructor(config: AxiosRequestConfig) {
+  constructor(config: FossRequestConfig) {
     this.requestInstance = axios.create(config)
+    this.interceptor = config.interceptors
+
+    this.requestInstance.interceptors.request.use(
+      this.interceptor?.requestInterceptor,
+      this.interceptor?.requestInterceptorCatch
+    )
+
+    this.requestInstance.interceptors.response.use(
+      this.interceptor?.reponseInterceptor,
+      this.interceptor?.reponseInterceptorCatch
+    )
   }
 
   request(config: AxiosRequestConfig): any {
