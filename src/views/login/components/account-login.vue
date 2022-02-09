568e474979b44c15816298196f16c976d5a71909
YDKD <1606354057@qq.com>
2022-02-09 20:43:46 +0800

    feat(login): add form

---
+++ src/views/login/components/account-login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="account-login">
+    <el-form
+      ref="ruleFormRef"
+      :model="ruleForm"
+      :rules="rules"
+      label-position="left"
+      label-width="80px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="用户名" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="ruleForm.password"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import type { ElForm } from 'element-plus'
+
+type FormInstance = InstanceType<typeof ElForm>
+
+const ruleFormRef = ref<FormInstance>()
+
+const ruleForm = reactive({
+  name: '',
+  password: ''
+})
+
+const rules = reactive({
+  name: [
+    {
+      required: true,
+      message: '请输入用户名',
+      trigger: 'blur'
+    },
+    {
+      min: 3,
+      max: 5,
+      message: '用户名长度为3-5',
+      trigger: 'blur'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: 'blur'
+    }
+  ]
+})
+</script>
+
+<style scoped></style>
--- src/views/login/components/login-panel.vue
+++ src/views/login/components/login-panel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login-panel">
+  <div class="login-panel mb-32">
     <h1 class="text-xl font-bold text-white text-center mb-2">后台系统</h1>
     <el-tabs type="border-card" class="demo-tabs" stretch>
       <el-tab-pane>
@@ -8,7 +8,7 @@
             <el-icon><UserFilled /></el-icon>账号登录
           </span>
         </template>
-        账号
+        <account-login />
       </el-tab-pane>
       <el-tab-pane>
         <template #label>
@@ -16,7 +16,7 @@
             <el-icon><Iphone /></el-icon>手机登录
           </span>
         </template>
-        手机
+        <phone-login />
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -24,10 +24,17 @@
 
 <script lang="ts" setup>
 import { UserFilled, Iphone } from '@element-plus/icons-vue'
+import accountLogin from './account-login.vue'
+import phoneLogin from './phone-login.vue'
 </script>
 
 <style scoped lang="less">
 .login-panel {
   width: 320px;
+  .demo-tabs > ::v-deep .el-tabs__content {
+    background-color: #f4f5f7;
+    color: #6b778c;
+    font-weight: 600;
+  }
 }
 </style>
