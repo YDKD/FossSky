b5ee423eb2e74d639481b27537d91edb2c2c7867
YDKD <1606354057@qq.com>
2022-03-04 16:49:48 +0800

    chore(commit): add custom commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Version: 1.0
+ * @Autor: YDKD
+ * @Date: 2022-03-04 16:49:18
+ * @LastEditors: YDKD
+ * @LastEditTime: 2022-03-04 16:49:19
+ */
 module.exports = {
   extends: ['@commitlint/config-conventional']
 }
