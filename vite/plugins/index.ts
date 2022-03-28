5f403e0e979b3817fe883fe250dd7f0f8c64801f
YDKD <1606354057@qq.com>
2022-03-28 15:22:56 +0800

    feat(icon): add pure-icons

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -2,8 +2,9 @@ import type { Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import autoImport from './autoImport'
+import PurgeIcons from 'vite-plugin-purge-icons'
 
 export default function setupPlugins() {
-  const plugins: Plugin[] = [vue(), vueJsx()]
+  const plugins: Plugin[] = [vue(), vueJsx(), PurgeIcons()]
   return [...plugins, ...autoImport()]
 }
