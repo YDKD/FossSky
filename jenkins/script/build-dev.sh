8b1595a0440f62169c05940539e5d13c8e9dd68c
YDKD <1606354057@qq.com>
2022-04-12 09:58:50 +0800

    build(jenkins): optimization memory manage

--- jenkins/script/build-dev.sh
+++ jenkins/script/build-dev.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 
 # set -x
-# stop nestjs
-docker stop nestjs
-
 npm run build-dev || exit 1
-
-# start nestjs
-docker start nestjs
 
 # set +x
---
+++ jenkins/script/stop.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# set -x
+# stop nestjs
+containerId=`docker ps -a | grep -w nestjs  | awk '{print $1}'`
+
+if [ "$containerId" !=  "" ] ; then
+	docker stop $containerId
+	echo "成功停止容器 $containerId"
+fi
+
+# stop mysql
+docker stop mysql
+
